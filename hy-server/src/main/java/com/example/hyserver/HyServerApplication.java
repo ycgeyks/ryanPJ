ec759b7eba3b294a3dd519cbcf16f4dcc9d927fd
Ryan <84768331@qq.com>
2020-07-13 02:23:38 +0800

    add Hystrix server

---
+++ hy-server/src/main/java/com/example/hyserver/HyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyServerApplication.class, args);
+	}
+
+}
