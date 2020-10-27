96025ab9459f1c48ba7539ba5036375e6aea4f16
Ryan <84768331@qq.com>
2020-10-27 11:06:20 +0800

    Update HyServerApplication.java

--- hy-server/src/main/java/com/example/hyserver/HyServerApplication.java
+++ hy-server/src/main/java/com/example/hyserver/HyServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HyServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HyServerApplication.class, args );
+		SpringApplication.run(HyServerApplication.class, args);
 	}
 
 }
