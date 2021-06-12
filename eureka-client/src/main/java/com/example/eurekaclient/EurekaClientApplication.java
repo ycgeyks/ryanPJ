41a083cc4b101712cb3515d48c35458db5b538d7
ryan <84768331@qq.com>
2021-06-13 01:23:48 +0800

    add eureka server and client modules

---
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,29 @@
+package com.example.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
+
+//@RestController
+//class ServiceInstanceRestController {
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @RequestMapping("/service-instances/{applicationName}")
+//    public List<ServiceInstance> serviceInstancesByApplicationName(
+//            @PathVariable String applicationName) {
+//        return this.discoveryClient.getInstances(applicationName);
+//    }
+//}
--- hy-server/src/main/java/com/example/hyserver/HyServerApplication.java
+++ hy-server/src/main/java/com/example/hyserver/HyServerApplication.java
@@ -2,12 +2,15 @@ package com.example.hyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class HyServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HyServerApplication.class, args);
+
+//		SpringApplication.run(HyServerApplication.class, args);
+		new SpringApplicationBuilder(HyServerApplication.class).properties("spring.config.location=classpath:/abc.properties").run(args);
 	}
 
 }
